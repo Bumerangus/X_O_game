149f86ac3748dad47b31e97a9fdafcd5418cceea
Bumerangus <vova7910@mail.ru>
2022-06-30 02:30:20 +0300

    Update X_O_game.py

--- X_O_game.py
+++ X_O_game.py
@@ -39,11 +39,11 @@ def print_start_field():
 
 
 def print_field():
-   index = 0
-   print(" ", 0, '', 1, '', 2)
-   for i in field_play:
-      print(index, "  ".join(i), end='\n')
-      index += 1
+    index = 0
+    print(" ", 0, '', 1, '', 2)
+    for i in field_play:
+        print(index, "  ".join(i), end='\n')
+        index += 1
 
 
 def replace_x():
@@ -113,63 +113,64 @@ def turn__o():
 
 
 def end():
-   answ = input("Еще разок?)\n(да/нет): ")
-   if answ == 'нет':
-       print("Слабак:-Р")
-      exit()
-   else:
-       game()
+    answ = input("Еще разок?)\n(да/нет): ")
+    if answ == 'нет':
+        print("Слабак:-Р")
+        exit()
+    else:
+        game()
 
 
 def check_win_x():
-   global win_x_str, win_x_stolb_1, win_x_stolb_2, win_x_stolb_3
-   global win_x_diag_1, win_x_diag_2
-   if xf[0] == 'X' and yf[0] == 'X' and zf[0] == 'X':
-      win_x_stolb_1 = True
-   elif xf[1] == 'X' and yf[1] == 'X' and zf[1] == 'X':
-      win_x_stolb_2 = True
-   elif xf[2] == 'X' and yf[2] == 'X' and zf[2] == 'X':
-      win_x_stolb_3 = True
-   elif xf[0] == 'X' and yf[1] == 'X' and zf[2] == 'X':
-      win_x_diag_1 = True
-   elif xf[2] == 'X' and yf[1] == 'X' and zf[0] == 'X':
-      win_x_diag_2 = True
-   if win_x_str[0] in field_play:
-      print(f'Крестики выйграли!({win_x_str[1]})\nПоздравляем!')
-      end()
-   elif win_x_stolb_1 or win_x_stolb_2 or win_x_stolb_3:
-      print(f'Крестики выйграли!(Крестики в столбик)\nПоздравляем!')
-      end()
-   elif win_x_diag_1 or win_x_diag_2:
-      print(f'Крестики выйграли по диагонали)\nПоздравляем!')
-      end()
+    global win_x_str, win_x_stolb_1, win_x_stolb_2, win_x_stolb_3
+    global win_x_diag_1, win_x_diag_2
+    if xf[0] == 'X' and yf[0] == 'X' and zf[0] == 'X':
+        win_x_stolb_1 = True
+    elif xf[1] == 'X' and yf[1] == 'X' and zf[1] == 'X':
+        win_x_stolb_2 = True
+    elif xf[2] == 'X' and yf[2] == 'X' and zf[2] == 'X':
+        win_x_stolb_3 = True
+    elif xf[0] == 'X' and yf[1] == 'X' and zf[2] == 'X':
+        win_x_diag_1 = True
+    elif xf[2] == 'X' and yf[1] == 'X' and zf[0] == 'X':
+        win_x_diag_2 = True
+    if win_x_str[0] in field_play:
+        print(f'Крестики выйграли!({win_x_str[1]})\nПоздравляем!')
+        end()
+    elif win_x_stolb_1 or win_x_stolb_2 or win_x_stolb_3:
+        print(f'Крестики выйграли!(Крестики в столбик)\nПоздравляем!')
+        end()
+    elif win_x_diag_1 or win_x_diag_2:
+        print(f'Крестики выйграли по диагонали)\nПоздравляем!')
+        end()
 
 
 def check_win_o():
-   global win_o_str, win_o_stolb_1, win_o_stolb_2, win_o_stolb_3
-   global win_o_diag_1, win_o_diag_2
-   if xf[0] == 'O' and yf[0] == 'O' and zf[0] == 'O':
-      win_o_stolb_1 = True
-   elif xf[1] == 'O' and yf[1] == 'O' and zf[1] == 'O':
-      win_o_stolb_2 = True
-   elif xf[2] == 'O' and yf[2] == 'O' and zf[2] == 'O':
-      win_o_stolb_3 = True
-   elif xf[0] == 'O' and yf[1] == 'O' and zf[2] == 'O':
-      win_o_diag_1 = True
-   elif xf[2] == 'O' and yf[1] == 'O' and zf[0] == 'O':
-      win_o_diag_2 = True
-   if win_o_str[0] in field_play:
-      print(f'Нолики выйграли!({win_o_str[1]})\nПоздравляем!')
-      end()
-   elif win_o_stolb_1 or win_o_stolb_2 or win_o_stolb_3:
-      print(f'Нолики выйграли!(Крестики в столбик)\nПоздравляем!')
-      end()
-   elif win_o_diag_1 or win_o_diag_2:
-      print(f'Нолики выйграли по диагонали)\nПоздравляем!')
-      end()
+    global win_o_str, win_o_stolb_1, win_o_stolb_2, win_o_stolb_3
+    global win_o_diag_1, win_o_diag_2
+    if xf[0] == 'O' and yf[0] == 'O' and zf[0] == 'O':
+        win_o_stolb_1 = True
+    elif xf[1] == 'O' and yf[1] == 'O' and zf[1] == 'O':
+        win_o_stolb_2 = True
+    elif xf[2] == 'O' and yf[2] == 'O' and zf[2] == 'O':
+        win_o_stolb_3 = True
+    elif xf[0] == 'O' and yf[1] == 'O' and zf[2] == 'O':
+        win_o_diag_1 = True
+    elif xf[2] == 'O' and yf[1] == 'O' and zf[0] == 'O':
+        win_o_diag_2 = True
+    if win_o_str[0] in field_play:
+        print(f'Нолики выйграли!({win_o_str[1]})\nПоздравляем!')
+        end()
+    elif win_o_stolb_1 or win_o_stolb_2 or win_o_stolb_3:
+        print(f'Нолики выйграли!(Крестики в столбик)\nПоздравляем!')
+        end()
+    elif win_o_diag_1 or win_o_diag_2:
+        print(f'Нолики выйграли по диагонали)\nПоздравляем!')
+        end()
 
 
 def game():
+    print("Приветствую, господа!\nПартейку в XO_Game?\nПусть победит сильнейший!")
     global x_input, o_input, turns, field_start, field_play, xf, yf, zf, win_x_str, win_o_str, win_o_stolb_2
     global win_x_stolb_1, win_x_stolb_2, win_x_stolb_3, win_x_diag_1, win_x_diag_2, win_o_stolb_1, win_o_stolb_3
     global win_o_diag_1, win_o_diag_2
